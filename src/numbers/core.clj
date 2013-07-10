d73b9d78d3931f86d2d4db5b90123fabdd348c69
Matthew Boston <matthew.boston@gmail.com>
2013-07-09 19:06:31 -0500

    initial commit

---
+++ src/numbers/core.clj
@@ -0,0 +1,60 @@
+;; TODO: map->pmap
+(ns numbers.core
+  (:require
+   [clojure.string :as str]
+   [clojure.java.io :refer [resource reader]]))
+
+(defn read-file
+  "reads a file of the specified format in the project spec"
+  [filename]
+  (let [rdr (reader (resource filename))]
+    (map (fn [line]
+           (map #(Integer/parseInt %)  (str/split line #",")))
+         (rest (line-seq rdr)) ;; eliminate header row
+         )))
+
+(defonce examples
+  [{:label nil
+    :pixels []}])
+
+(defn square [^Integer n] (* n n))
+(defn cubed [^Integer n] (* n n n))
+
+(defn square-of-difference [x y] (square (- x y)))
+
+(defn euclidean-distance
+  "calculates the Euclidean Distance between two vectors"
+  [x y]
+  (reduce + (map square-of-difference x y)))
+
+(defn dist [x y] (euclidean-distance x y))
+
+(defn closest-neighbor
+  ""
+  ([knowns unknown]
+     (closest-neighbor (rest knowns)
+                       unknown
+                       (dist (first knowns) unknown)
+                       (first knowns)))
+  ([knowns unknown best-score best-match]
+     (if (seq knowns)
+       (let [score (dist (first knowns) unknown)]
+         (if (< score best-score)
+           (recur (rest knowns) unknown score (first knowns))
+           (recur (rest knowns) unknown best-score best-match)))
+       {:best-score best-score
+        :best-match best-match})))
+
+(defn -main [& args]
+  (map (partial closest-neighbor examples)))
+
+(defn accuracy
+  "calculates accuracy metrics for the closest-neighbor function"
+  []
+  {:total 0
+   :correct 0})
+
+(defn performance
+  "calculates performance metrics for the closest-neighbor function"
+  []
+  (time (closest-neighbor [] [])))
