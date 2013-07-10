9993fbe58f20e34e402e034168c44e1d8c6277be
Matthew Boston <matthew.boston@gmail.com>
2013-07-09 23:05:32 -0500

    add some tests

--- test/numbers/core_test.clj
+++ test/numbers/core_test.clj
@@ -2,6 +2,25 @@
   (:require [clojure.test :refer :all]
             [numbers.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest split-to-integers-test
+  (testing "spliting comma separated strings into integers"
+    (is (= '(1 2 3) (split-to-integers "1,2,3")))))
+
+(deftest read-file-test)
+
+(deftest parse-file-test)
+
+(deftest square-test
+  (testing "squaring a number"
+    (is (= 1 (square 1)))
+    (is (= 16 (square 4)))))
+
+(deftest cubed-test)
+
+(deftest square-of-difference-test
+  (testing "squares the difference of two numbers"
+    (is (= 16 (square-of-difference 9 5)))))
+
+(deftest euclidean-distance-test)
+
+(deftest closest-neighbor-test)
