836c97946502331f1f98b6c2ec442e33d24b4339
Matthew Boston <matthew.boston@gmail.com>
2013-07-18 23:07:55 -0400

    test for finding the closest neighbor that isn't an exact match

--- test/numbers/core_test.clj
+++ test/numbers/core_test.clj
@@ -10,4 +10,10 @@
     (let [knowns (list {:label 1 :pixels (list 9 0 9 9 0 9 9 0 9)}
                        {:label 4 :pixels (list 0 9 0 0 0 0 9 9 0)})
           unknown {:label 1 :pixels (list 9 0 9 9 0 9 9 0 9)}]
-      (is (= {:best-score 0 :best-match {:label 1 :pixels '(9 0 9 9 0 9 9 0 9)}} (closest-neighbor knowns unknown))))))
+      (is (= {:best-score 0 :best-match {:label 1 :pixels '(9 0 9 9 0 9 9 0 9)}} (closest-neighbor knowns unknown)))))
+
+  (testing "returns the closest neighbor for a non-exact match"
+    (let [knowns (list {:label 1 :pixels (list 9 0 9 9 0 9 9 0 9)}
+                       {:label 4 :pixels (list 0 9 0 0 0 0 9 9 0)})
+          unknown {:label 1 :pixels (list 6 1 6 6 0 6 6 0 6)}]
+      (is (= {:best-score 55 :best-match {:label 1 :pixels '(9 0 9 9 0 9 9 0 9)}} (closest-neighbor knowns unknown))))))
