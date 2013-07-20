1898bfa673d53e4b955457517514a188f402975b
Matthew Boston <matthew.boston@gmail.com>
2013-07-20 14:32:11 -0400

    prepare the closest neighbor functions for getting back a list of the nth closest neighbors

--- src/numbers/core.clj
+++ src/numbers/core.clj
@@ -5,16 +5,27 @@
 
 (defn dist [x y] (euclidean-distance x y))
 
-(defn closest-neighbor
-  "a machine learning classifier which identifies the closest neighbor
-   based on a distance function."
+(defn closest-neighbors
+  "a machine learning classifier which returns a lazy-seq of the
+   closest neighbors sorted by distance."
   ([unknown knowns]
-     (closest-neighbor unknown knowns Integer/MAX_VALUE {}))
+     (closest-neighbors unknown knowns Integer/MAX_VALUE {}))
   ([unknown knowns best-score best-match]
      (if (and (seq knowns) (not (zero? best-score)))
        (let [score (dist (:pixels (first knowns)) unknown)]
          (if (< score best-score)
            (recur unknown (rest knowns) score (first knowns))
            (recur unknown (rest knowns) best-score best-match)))
-       {:best-score best-score
-        :best-match best-match})))
+       (list {:best-score best-score
+              :best-match best-match}))))
+
+(defn nth-closest-neighbor
+  ""
+  [n unknown knowns]
+  (take n (closest-neighbors unknown knowns)))
+
+(defn closest-neighbor
+  "a machine learning classifier which identifies the closest neighbor
+   based on a distance function."
+  [unknown knowns]
+  (first (closest-neighbors unknown knowns)))
