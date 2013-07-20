b82aa0f64af3d8029c8492645a49a8c271d5fef2
Matthew Boston <matthew.boston@gmail.com>
2013-07-20 14:25:33 -0400

    add stepped accuracy to show how increasing example sizes affects correctness percentage

--- src/numbers/metrics.clj
+++ src/numbers/metrics.clj
@@ -26,6 +26,20 @@
         :percentage (float (if (zero? total) 0 (/ correct total)))
         :trained-on (count trainers)})))
 
+(defn stepped-accuracy
+  "steps through the accuracy function loading different sizes of data sets"
+  []
+  (do
+    (println (accuracy (take 100 validation-set) (take 500 training-set)))
+    (println (accuracy (take 100 validation-set) (take 1000 training-set)))
+    (println (accuracy (take 100 validation-set) (take 3000 training-set)))
+    (println (accuracy (take 100 validation-set) training-set))
+    (println (accuracy validation-set (take 500 training-set)))
+    (println (accuracy validation-set (take 1000 training-set)))
+    (println (accuracy validation-set (take 2000 training-set)))
+    (println (accuracy validation-set (take 3000 training-set)))
+    (println (accuracy validation-set training-set))))
+
 (defn performance
   "calculates performance metrics for the closest-neighbor function"
   []
